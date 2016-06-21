5a44ac23a0e749eae5e0a344dca2aa2fe15ec9a4
hanks-zyh <zhangyuhan2014@gmail.com>
2016-06-21 23:57:51 +0800

    finish send SMS

--- app/src/main/java/xyz/hanks/notifyme/MainActivity.java
+++ app/src/main/java/xyz/hanks/notifyme/MainActivity.java
@@ -3,7 +3,8 @@ package xyz.hanks.notifyme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+
+import cn.smssdk.SMSSDK;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SMSSDK.initSDK(this, "1426505bd0b02", "4abf2e726b1db62b0389712a96b32398");
+        startIntentService();
     }
 
 
-    public void startIntentService(View source) {
+    public void startIntentService() {
         // 创建需要启动的IntentService的Intent
-        Intent intent = new Intent(this, NetCheckIntentService.class);
+        Intent intent = new Intent(this, NetCheckService.class);
         startService(intent);
     }
 }
